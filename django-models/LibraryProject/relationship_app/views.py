a3d9326439f035cfbc61f9cc53b82b384ff8eaa5
olami851 <abdhamid851@gmail.com>
2025-02-26 20:55:13 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -23,6 +23,6 @@ class LibraryDetailView(DetailView):
     context_object_name = 'library'
     
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'    
