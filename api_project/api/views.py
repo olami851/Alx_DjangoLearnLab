4db4cf2f551f98a06ead10ac389e3ee041866b38
olami851 <abdhamid851@gmail.com>
2025-03-03 15:56:32 +0100

    Building Your First API Endpoint with Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+# from rest_framework import generics
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
