500494d6a245618e7b2c5535e703ccf4d7cdf680
olami851 <abdhamid851@gmail.com>
2025-03-30 16:26:17 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import UserRegistrationView
+from .views import UserLoginView
+from .views import UserDetailView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', )
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserDetailView.as_view(), name='profile')
 ]
