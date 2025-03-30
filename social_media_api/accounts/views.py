9c6e7674bf731294da0654bf68e5464653a84ddc
olami851 <abdhamid851@gmail.com>
2025-03-30 22:26:08 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import status, generics
+from rest_framework import status
+from rest_framework import generics
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,6 +10,7 @@ from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 from .models import CustomUser
+from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 # import RefreshToken # type: ignore
 # from rest_framework_simplejwt.token import RefreshToken
